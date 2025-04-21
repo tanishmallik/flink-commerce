d57325e15076fdb7bd6518c9f6b20bb2e61ca9a9
Tanish Mallik <111840299+tanishmallik@users.noreply.github.com>
2025-04-21 20:38:59 +0530

    Add files via upload

---
+++ main/java/FlinkCommerce/DataStreamJob.java
@@ -0,0 +1,35 @@
+package FlinkCommerce;
+
+import Deserializer.JSONValueDeserializationSchema;
+import Dto.Transaction;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class DataStreamJob {
+
+	public static void main(String[] args) throws Exception {
+		// Sets up the execution environment, which is the main entry point
+		// to building Flink applications.
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		String topic = "financial_transactions";
+
+		KafkaSource<Transaction> source = KafkaSource.<Transaction>builder()
+				.setBootstrapServers("localhost:9092")
+				.setTopics(topic)
+				.setGroupId("flink-group")
+				.setStartingOffsets(OffsetsInitializer.earliest())
+				.setValueOnlyDeserializer(new JSONValueDeserializationSchema())
+				.build();
+
+		DataStream<Transaction> transactionStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
+
+		transactionStream.print();
+
+		// Execute program, beginning computation.
+		env.execute("Flink Java API Skeleton");
+	}
+}
