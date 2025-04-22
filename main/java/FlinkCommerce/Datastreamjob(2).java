8ec4159210d544791cf4f36503a6ca32d916bb14
Tanish Mallik <111840299+tanishmallik@users.noreply.github.com>
2025-04-22 17:08:54 +0530

    Create Datastreamjob(2).java

---
+++ main/java/FlinkCommerce/Datastreamjob(2).java
@@ -0,0 +1,43 @@
+package FlinkCommerce;
+
+import Deserializer.JSONValueDeserializationSchema;
+import Dto.Transaction;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class DataStreamJob {
+
+	public static void main(String[] args) throws Exception {
+		// Sets up the execution environment, which is the main entry point
+		// to building Flink applications.
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		String topic = "financial_transactions";
+
+		KafkaSource<Transaction> source = KafkaSource.<Transaction>builder()
+				.setBootstrapServers("broker:29092")
+				.setTopics(topic)
+				.setGroupId("flink-group")
+				.setStartingOffsets(OffsetsInitializer.earliest())
+				.setValueOnlyDeserializer(new JSONValueDeserializationSchema())
+				.build();
+
+		DataStream<Transaction> transactionStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
+
+		transactionStream.map(transaction -> {
+			System.out.println("tanish123");
+			return transaction;
+		})
+				.print();
+
+    //		transactionStream.map(transaction -> {
+		//	System.out.println("tanish123");
+			//return transaction;
+	//	});
+		// Execute program, beginning computation.
+		env.execute("Flink Java API Skeleton");
+	}
+}
